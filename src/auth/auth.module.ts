9b5217d911de004f3430fbdf45f5a8dbd154cd4e
thiagoDEVasconcelos <thiagovsousa21@gmail.com>
2026-03-28 19:11:12 -0300

    Creating the authService.

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,17 @@ import { HashingService } from './hashing/hashing.service';
 import { BcryptService } from './hashing/bcrypt.service';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Users } from 'src/users/entities/user.entity';
+import { ConfigModule } from '@nestjs/config';
+import jwtConfig from './config/jwt.config';
 
 @Global()
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Users]),
+    ConfigModule.forFeature(jwtConfig),
+  ],
   controllers: [AuthController],
   providers: [
     {
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,42 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable, UnauthorizedException } from '@nestjs/common';
 import { LoginDto } from './dto/loginDto';
+import { Repository } from 'typeorm';
+import { Users } from 'src/users/entities/user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { HashingService } from './hashing/hashing.service';
+import jwtConfig from './config/jwt.config';
+import type { ConfigType } from '@nestjs/config';
 
 @Injectable()
 export class AuthService {
-  login(loginDto: LoginDto) {
-    return loginDto;
+  constructor(
+    @InjectRepository(Users)
+    private readonly usersRepository: Repository<Users>,
+    private readonly hashingService: HashingService,
+    @Inject(jwtConfig.KEY)
+    private readonly jwtConfiguration: ConfigType<typeof jwtConfig>,
+  ) {
+    console.log(this.jwtConfiguration);
+  }
+
+  async login(loginDto: LoginDto) {
+    let passwordIsValid = false;
+
+    const user = await this.usersRepository.findOne({
+      where: { email: loginDto.email },
+    });
+
+    if (user) {
+      passwordIsValid = await this.hashingService.compare(
+        user.password,
+        loginDto.password,
+      );
+    }
+
+    if (!passwordIsValid) {
+      throw new UnauthorizedException('Invalid Password!');
+    }
+
+    return user;
   }
 }
