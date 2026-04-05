bd059c43356eda45adda88ee7006cc36b426c284
thiagoDEVasconcelos <thiagovsousa21@gmail.com>
2026-04-04 23:54:10 -0300

    Refactoring the RoutePolicyGuard.

--- src/auth/guards/route-policy.guard.ts
+++ src/auth/guards/route-policy.guard.ts
@@ -1,12 +1,17 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { ROUTE_POLICY_KEY } from '../auth.constants';
+import { RoutePolicies } from '../enums/route-policies.enum';
 
 @Injectable()
 export class RoutePolicyGuard implements CanActivate {
   constructor(private readonly reflector: Reflector) {}
   canActivate(context: ExecutionContext): boolean {
-    console.log(this.reflector.get(ROUTE_POLICY_KEY, context.getHandler()));
+    const routePolicyRequired = this.reflector.get<RoutePolicies | undefined>(
+      ROUTE_POLICY_KEY,
+      context.getHandler(),
+    );
+    console.log(routePolicyRequired);
     return true;
   }
 }
