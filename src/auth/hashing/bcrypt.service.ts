8da209dd927ade95f3aac3f73c60fed7ef5ef245
thiagoDEVasconcelos <thiagovsousa21@gmail.com>
2026-05-01 14:25:48 -0300

    Create the e2e test base files.

--- src/auth/hashing/bcrypt.service.ts
+++ src/auth/hashing/bcrypt.service.ts
@@ -1,4 +1,4 @@
-import bcrypt from 'node_modules/bcryptjs';
+import bcrypt from 'bcryptjs';
 import { HashingService } from './hashing.service';
 
 export class BcryptService extends HashingService {
