9541c54d26c24c292de2e5f7ce1e5be4aab7c0ac
thiagoDEVasconcelos <thiagovsousa21@gmail.com>
2026-05-03 02:18:34 -0300

    Using helmet and cors.

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ParseIntIdPipe } from './common/pipes/parse-int-id.pipe';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(helmet());
+  app.enableCors({
+    origin: 'https://myapp.com',
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
